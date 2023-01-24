f7d5a194e947bcd8048b1ea3713688df2cbc643b
Basil Njoga <Basilnjoga@gmail.com>
2023-01-24 23:59:04 +0300

    added promise protoype to function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,11 @@
-let p = new Promise(function getResponseFromAPI()){
-
-};
+export default function getResponseFromAPI() {
+  const promise = new Promise(((resolve, reject) => {
+    const a = 1 + 1;
+    if (a === 2) {
+      resolve('Stuff worked!');
+    } else {
+      reject(Error('It broke'));
+    }
+  }));
+  return promise;
+}
