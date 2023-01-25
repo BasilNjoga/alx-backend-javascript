071e6c1111469b28bdd256cb884396ef935fa91a
Basil Njoga <Basilnjoga@gmail.com>
2023-01-25 14:21:51 +0300

    check

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,12 @@
-function getFullResponseFromAPI(success) {
-  if(success) {
-    resolve("status: 200\nbody: 'Success'")
-  } else {
-    reject("The fake API is not working currently")
-  }
-
+export default function getFullResponseFromAPI(success) {
+  const p = new Promise((resolve, reject) => {
+    if (success) {
+      resolve('stuff workded');
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+  return p;
 }
+
+console.log(getFullResponseFromAPI(false));
