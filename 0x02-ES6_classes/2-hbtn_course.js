a469a8fd574b7b9bf5a41f244f851cd75ced4553
Basil Njoga <Basilnjoga@gmail.com>
2023-01-27 00:32:03 +0300

    chekc

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,19 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
+
+  /*
+  get name() {
+    return this.name;
+  }
+  get length() {
+    return this.length;
+  }
+  get students() {
+    return this.students;
+  }
+  */
 }
