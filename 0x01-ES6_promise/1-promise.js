189b624f0fb5f03f3d56b2fa9afbbfde91f190c1
Basil Njoga <Basilnjoga@gmail.com>
2023-01-28 01:14:04 +0300

    added then functions

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve('Success');
-    } else {
-      reject(Error('The fake API is not working currently'));
-    }
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
   });
-  p.then(("status: 200\nbody: 'Success'")).catch(('The fake API is not working currently'));
-
 }
