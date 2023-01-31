60348873f47b6e04a4cb574f3dab0a37fc63f153
Basil Njoga <Basilnjoga@gmail.com>
2023-01-31 12:36:02 +0300

    set correct attributes

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-export default function getListStudents(id, firstname, location) {
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillame', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
