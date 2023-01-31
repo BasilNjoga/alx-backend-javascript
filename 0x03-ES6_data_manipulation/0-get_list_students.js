6cba1c7cc90cec515deab36cb7ebb9dc26a4ef3a
Basil Njoga <Basilnjoga@gmail.com>
2023-01-31 12:05:01 +0300

    added object

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents(id, firstname, location) {
-
-    return [{
-        id: id,
-        firstname: firstname,
-        location:location
-    }]
+  return [
+    { id: 1, firstName: 'Guillame', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
